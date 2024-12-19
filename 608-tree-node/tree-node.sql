5af00d3ad4104826be42f71e6adfc81acf86f035
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2024-12-19 00:12:19 -0500

    Time: 808 ms (11.68%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -2,19 +2,12 @@
 
 
 
-WITH NodeTypes AS (
-    SELECT
-        t.id,
-        CASE
-            WHEN t.p_id IS NULL THEN 'Root'
-            WHEN NOT EXISTS (
-                SELECT 1
-                FROM Tree t2
-                WHERE t2.p_id = t.id
-            ) THEN 'Leaf'
-            ELSE 'Inner'
-        END AS node_type
-    FROM Tree t
-)
-SELECT id, node_type as type
-FROM NodeTypes;
+SELECT id,
+
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
+        ELSE 'Leaf'
+        END AS type
+ FROM Tree
+		
