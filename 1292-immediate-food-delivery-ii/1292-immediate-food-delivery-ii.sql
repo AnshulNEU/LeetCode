85536f235535be79536db6d81460dca4e3d9fb67
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-01-07 01:36:09 -0500

    Time: 689 ms (67.08%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 
+
 with min_order_date as 
 (select min(order_date) as order_date, customer_id from delivery group by customer_id)
 
@@ -7,3 +8,8 @@ select Round((count(d.delivery_id)/(select count(*) from min_order_date))*100, 2
 from delivery d left join min_order_date m 
 on d.order_date=m.order_date and d.customer_id=m.customer_id
 where m.order_date=d.customer_pref_delivery_date
+
+
+
+
+
