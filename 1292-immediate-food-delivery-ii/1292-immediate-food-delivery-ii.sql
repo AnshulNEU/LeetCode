3ac323fab95ff9b7827ecf18b99b23387ef5365d
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-01-07 01:36:26 -0500

    Time: 1077 ms (30.47%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -5,7 +5,7 @@ with min_order_date as
 (select min(order_date) as order_date, customer_id from delivery group by customer_id)
 
 select Round((count(d.delivery_id)/(select count(*) from min_order_date))*100, 2) as immediate_percentage
-from delivery d left join min_order_date m 
+from delivery d join min_order_date m 
 on d.order_date=m.order_date and d.customer_id=m.customer_id
 where m.order_date=d.customer_pref_delivery_date
 
