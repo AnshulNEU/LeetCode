36c920886d490215ec3133bd217a7f9e53e70c27
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-03-03 02:13:53 -0500

    Time: 2405 ms (19.93%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -2,24 +2,22 @@
 
 
 
+# Write your MySQL query statement below
+
 WITH RankedSalaries AS (
     SELECT 
-        e.id, 
-        e.name, 
-        e.salary, 
-        e.departmentId, 
-        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS top
-    FROM 
-        Employee e
+        e.name AS Employee,
+        d.name AS Department,
+        e.salary,
+        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
 )
-SELECT 
-    d.name department,
-    rs.name employee, 
-    rs.salary
-FROM 
-    RankedSalaries rs join Department d
-    on rs.departmentId = d.id
-WHERE 
-    rs.top <= 3;
+SELECT Department, Employee, Salary
+FROM RankedSalaries
+WHERE rnk <= 3;
+
+
+
 
 
