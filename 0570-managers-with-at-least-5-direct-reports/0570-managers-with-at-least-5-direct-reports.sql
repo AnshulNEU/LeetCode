e30732bf033582f16fdc789704503a8d404e08fd
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-04-24 12:47:01 -0400

    Time: 409 ms (55.51%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 
+select a.name from
+(select e.id, e.name, count(e.id) as cun
+from employee e join employee ep on e.id=ep.managerId
+where ep.managerid is not null
+group by id) as a
+where cun >= 5
 
-
-select e.name
-from employee e join employee em
-on e.id = em.managerid
-group by e.id
-having count(em.managerid)>=5
