179f1c728c7b22f0479673457435738617e1873d
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-04-24 13:12:49 -0400

    Time: 631 ms (91.07%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,13 +1,7 @@
 # Write your MySQL query statement below
 
-
 select s.user_id, 
-    coalesce(Round(sum(case when c.action = 'confirmed' then 1 else 0 end)/count(c.action),2),0) confirmation_rate
+Coalesce(Round(sum(case when c.action = 'confirmed' then 1 else 0 end)/count(c.action),2), 0) as confirmation_rate
 from signups s left join confirmations c
 on s.user_id = c.user_id
 group by s.user_id
-
-
-
-#no action = 0
-#time-out =
