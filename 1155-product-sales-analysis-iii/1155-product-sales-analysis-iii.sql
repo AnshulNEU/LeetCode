2fc48bd41e878de66b87355fbbb1916f2c9f167d
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-04-24 17:50:45 -0400

    Time: 908 ms (98.66%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,16 +1,9 @@
 # Write your MySQL query statement below
 
-select s.product_id, p.first_year, s.quantity, price
-from sales s join (select product_id, min(year) first_year from sales group by product_id) p
-on s.product_id=p.product_id and s.year=p.first_year
 
-
-/*
-select s.product_id, s.year as first_year, s.quantity, s.price
-from sales s join (select product_id, min(year) as firstyear from sales group by product_id) as p2
-on s.product_id = p2.product_id and s.year=p2.firstyear
-*/
-
-
-
-
+with cte as (
+    select product_id, min(year) as first_year from Sales
+    group by product_id
+)
+select c.product_id, c.first_year, s.quantity, s.price
+from cte c join sales s on c.product_id = s.product_id and c.first_year=s.year
