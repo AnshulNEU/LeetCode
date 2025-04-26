13fe57a9c9a26d896d6dbc81e83cde3523dbc82d
Anshul Singh <114614768+AnshulNEU@users.noreply.github.com>
2025-04-25 23:22:55 -0400

    Time: 1473 ms (89.2%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,31 +1,9 @@
-# Write your MySQL query statement below
 
-Select category, accounts_count
-From
-(Select 
-    'Low Salary' as category, Count(account_id) accounts_count
-From    
-    Accounts
-Where 
-    income < 20000
-
-Union All
-
-Select 
-    'Average Salary' as category, Count(account_id) accounts_count
-From    
-    Accounts
-Where 
-    income Between 20000 and 50000
-
-Union All
-
-Select 
-    'High Salary' as category, Count(account_id) accounts_count
-From    
-    Accounts
-Where 
-    income > 50000) p
-
-order by accounts_count desc
-
+select 'Low Salary' as category,coalesce(Count(account_id),0) as accounts_count from
+    Accounts where income<20000
+Union
+select 'Average Salary' as category,coalesce(Count(account_id),0) as accounts_count from
+    Accounts where income between 20000 and 50000
+Union
+select 'High Salary' as category,coalesce(Count(account_id),0) as accounts_count from
+    Accounts where income >50000
